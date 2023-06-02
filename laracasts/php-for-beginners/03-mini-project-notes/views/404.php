042121fab522109cae0da45b5b0dbdf9b0a38aed
Dennis Groenendijk <info@dennis-groenendijk.com>
2023-06-03 00:38:14 +0200

    Update section 03

--- laracasts/php-for-beginners/03-mini-project-notes/views/404.php
+++ laracasts/php-for-beginners/03-mini-project-notes/views/404.php
@@ -5,7 +5,7 @@
         <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
             <h1 class="text-2xl font-bold">Sorry. Page Not Found.</h1>
             <p class="mt-4">
-                <a href="/" class="text-blue underline">Go back to Home page.</a>
+                <a href="/" class="text-blue-500 hover:underline">Go back to Home page.</a>
             </p>
         </div>
     </main>
--- laracasts/php-for-beginners/03-mini-project-notes/views/partials/head.php
+++ laracasts/php-for-beginners/03-mini-project-notes/views/partials/head.php
@@ -2,7 +2,7 @@
 <html lang="en" class="h-full bg-gray-100">
 <head>
     <meta charset="UTF-8">
-    <title>Section 02</title>
+    <title>Section 03</title>
     <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
 </head>
 
